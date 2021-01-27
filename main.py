3639645ae10bdacb52fe414bba2bccac2564a04f
Ali Mohamed <alimohamadyoussef21@gmail.com>
2021-01-27 14:10:31 +0200

    small change

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import json
+import sys
 
 import tweepy
 
-user = input('Please enter the twitter account handle:')
+user = input('Please enter the twitter account username:')
 data = json.load(open('D:\passwords.json'))
 auth = tweepy.OAuthHandler(data['API_key'], data['API_secret_key'])
 auth.set_access_token(data['access_token'], data['access_token_secret'])
 api = tweepy.API(auth)
-user_timeline = api.user_timeline(user, include_rts=False, count=2000, exclude_replies=True)
+try:
+    user_timeline = api.user_timeline(user, include_rts=False, count=2000, exclude_replies=True)
+except:
+    print('Invalid username')
+    sys.exit(1)
 total_likes = 0
 total_rts = 0
 tweets_count = len(user_timeline)
@@ -15,5 +20,5 @@ for tweet in user_timeline:
     total_likes += tweet.favorite_count
     total_rts += tweet.retweet_count
 
-print('average likes per tweet:{}'.format(round(total_likes / tweets_count)))
-print('average retweets per tweet:{}'.format(round(total_rts / tweets_count)))
+print(f'average likes per tweet:{round(total_likes / tweets_count)}')
+print(f'average retweets per tweet:{round(total_rts / tweets_count)}')
